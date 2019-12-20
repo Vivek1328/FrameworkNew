662fcfca50bbeea6f597811713971e8f4a8e9ffc
vivek1328 <rits.chaudhary1992@gmail.com>
2019-12-20 17:56:53 +0530

    Test

--- TA-CPM/src/test/java/testpackage/Cpm_Test_login.java
+++ TA-CPM/src/test/java/testpackage/Cpm_Test_login.java
@@ -12,7 +12,7 @@ public class Cpm_Test_login extends Utility_Baseclass {
 
 	
 	
-	//This is test
+	
 	CommonExcel excel= new CommonExcel();
 	@Test
 	public void getLigin_details() throws Exception {
